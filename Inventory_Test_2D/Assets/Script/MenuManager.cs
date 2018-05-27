513b91a2448dcb49be7708ff1410cb7fbbb73166
octopopo <godpopo34@gmail.com>
2018-05-27 13:12:24 -0700

    Added Menu Script
    Wrtite a simple script for menu manager

---
+++ Inventory_Test_2D/Assets/Script/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public delegate void AddingButtonHandler();
+    public static event AddingButtonHandler buttonAdded;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ItemAdding()
+    {
+
+    }
+}
